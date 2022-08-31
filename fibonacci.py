9c87bc2b031761eac5f71c9ddf2982aa17ab0d5e
AmolShahh <amol@nirenshah.com>
2022-08-31 10:12:31 -0500

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,8 @@
 import datetime
 import yfinance as yf
 import time
+import smtplib
+import ssl
 
 def calcRetracements(swing):
     level = .5
@@ -13,7 +15,16 @@ def getData():
         time.sleep(2)
     return data
 
-ticker = "BTC-USD"
+smtp_server = "smtp.gmail.com"
+sender_email = "FibonacciiRetracement@gmail.com"
+password = "fibon@acci!"
+port = 587
+reciever_email = "amol@nirenshah.com"
+message = "SPY has hit 0.5 Level"
+context = ssl.create_default_context()
+
+
+ticker = "SPY"
 dayStart = str(datetime.datetime.now().date()) + " 08:00:00"
 start = str(datetime.datetime.now().date()) + " 08:01:00"
 end = str(datetime.datetime.now().date()) + " 15:30:00"
@@ -58,8 +69,32 @@ while str(datetime.datetime.now()) > start and str(datetime.datetime.now()) < en
             min = current
             max = current_max
             swing = 0
+            try:
+                server = smtplib.SMTP(smtp_server,port)
+                server.ehlo() # Can be omitted
+                server.starttls(context=context) # Secure the connection
+                server.ehlo() # Can be omitted
+                server.login(sender_email, password)
+                server.sendmail(sender_email, reciever_email, message)
+            except Exception as e:
+                # Print any error messages to stdout
+                print(e)
+            finally:
+                server.quit()
         else:
             print("\\\\\\\\\\\\\\\\\\\\\\\Hit but not enough swing////////////////////////")
+            try:
+                server = smtplib.SMTP(smtp_server,port)
+                server.ehlo() # Can be omitted
+                server.starttls(context=context) # Secure the connection
+                server.ehlo() # Can be omitted
+                server.login(sender_email, password)
+                server.sendmail(sender_email, reciever_email, "Hit 0.5 level without enough swing")
+            except Exception as e:
+                # Print any error messages to stdout
+                print(e)
+            finally:
+                server.quit()
 
         
         
