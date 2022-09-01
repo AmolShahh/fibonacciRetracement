0df90e3fb8e3564679b4424acff0d763fd62b26a
AmolShahh <amol@nirenshah.com>
2022-08-31 20:50:11 -0500

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -40,54 +40,18 @@ def send_level_email():
         Source="FibonacciiRetracement@gmail.com",
     )
 
-def send_email_low_swing():
-    ses_client = boto3.client("ses", region_name="us-east-1")
-    CHARSET = "UTF-8"
-
-    response = ses_client.send_email(
-        Destination={
-            "ToAddresses": [
-                "amol@nirenshah.com",
-            ],
-        },
-        Message={
-            "Body": {
-                "Text": {
-                    "Charset": CHARSET,
-                    "Data": "SPY has hit 0.5 level WITHOUT enough swing",
-                }
-            },
-            "Subject": {
-                "Charset": CHARSET,
-                "Data": "SPY Fibonacci Retracement tracker",
-            },
-        },
-        Source="FibonacciiRetracement@gmail.com",
-    )
-
 
 ticker = "SPY"
-dayStart = str(datetime.datetime.now().date()) + " 08:00:00"
-start = str(datetime.datetime.now().date()) + " 08:01:00"
-end = str(datetime.datetime.now().date()) + " 15:30:00"
-
 level = 0
-#swap this into a do while instead of an if + a while?
-if str(datetime.datetime.now()) == dayStart:
-       data = getData()
-       min = data[-1:]['Low']
-       max = data[-1:]['High']
-       swing = max[0] - min[0]
-       level = calcRetracements(swing)
 
 data = getData()
 min = data[-1:]['Low']
 max = data[-1:]['High']
 swing = max[0] - min[0]
 
-isTrue = str(datetime.datetime.now()) > start and str(datetime.datetime.now()) < end
 
-while isTrue:
+
+while True:
     data = getData()
     current = data[-1:]['Low']
     current_max = data[-1:]['High']
@@ -96,13 +60,14 @@ while isTrue:
     # print("--------")
     if min[0] > current[0]:
         min = current
+        max = current_max
         swing = max[0] - min[0]
         level = calcRetracements(swing)
     elif max[0] < current_max[0]:
         max = current_max
         swing = max[0] - min[0]
         level = calcRetracements(swing)
-    
+    print(level)
     # print(current)
     # print(level)
     # print(min)
