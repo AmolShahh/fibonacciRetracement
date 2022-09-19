949ba79113d4a5edac90b65f149d7598b08d66fd
AmolShahh <amol@nirenshah.com>
2022-09-19 16:25:45 -0500

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,25 @@
 import yfinance as yf
 import time
 import boto3
+from datetime import date
+import datetime
 
-
+# These first 3 functions simply calculate the level for when notifications need to be sent out
+# The second 2 are only called once the retracement level is hit (when stock is bought)
+# For improved functionality, make this into 1 function and return an array
 def calcRetracements(swing):
     level = .5
     return max[0] - level*swing
 
+def calcSellRetracement():
+    level = 0.236
+    return max[0] - level*swing
+
+def calcLossRetracement():
+    level = 0.786
+    return max[0] - level*swing
+
+# Get data from yahoo finance API using yfinance. If the data fails to download, tries again in 2 seconds
 def getData():
     data = yf.download(tickers=ticker, period="5m", interval="5m")
     while(len(data) == 0 or data is None):
@@ -14,7 +27,9 @@ def getData():
         time.sleep(2)
     return data
 
-def send_level_email(stock_ticker, price, min, max):
+# These send the buy/sell/loss emails to anyone who is added to the list
+# Realistically, should condense this down into 1 function that takes a parameter to send buy/sell/loss mail
+def send_buy_email(stock_ticker, price, min, max):
     ses_client = boto3.client("ses", region_name="us-east-1")
     CHARSET = "UTF-8"
     DATA = str(stock_ticker) + " has hit 0.5 level at price $" + str(price) + ". \nMin: " + str(min) + "\nMax: " + str(max)
@@ -33,18 +48,102 @@ def send_level_email(stock_ticker, price, min, max):
             },
             "Subject": {
                 "Charset": CHARSET,
-                "Data": "SPY Fibonacci Retracement tracker",
+                "Data": "SPY Fibonacci Retracement Tracker",
             },
         },
         Source="FibonacciiRetracement@gmail.com",
     )
 
+def send_sell_email(stock_ticker, price):
+    ses_client = boto3.client("ses", region_name="us-east-1")
+    CHARSET = "UTF-8"
+    DATA = str(stock_ticker) + " has hit sell level at price $" + str(price) + "."
+    response = ses_client.send_email(
+        Destination={
+            "ToAddresses": [
+                "amol@nirenshah.com", "reviewstopclass@gmail.com",
+            ],
+        },
+        Message={
+            "Body": {
+                "Text": {
+                    "Charset": CHARSET,
+                    "Data": DATA,
+                }
+            },
+            "Subject": {
+                "Charset": CHARSET,
+                "Data": "SPY Fibonacci Retracement Tracker",
+            },
+        },
+        Source="FibonacciiRetracement@gmail.com",
+    )
+
+def send_loss_email(stock_ticker, price):
+    ses_client = boto3.client("ses", region_name="us-east-1")
+    CHARSET = "UTF-8"
+    DATA = str(stock_ticker) + " has hit loss level at price $" + str(price) + "."
+    response = ses_client.send_email(
+        Destination={
+            "ToAddresses": [
+                "amol@nirenshah.com", "reviewstopclass@gmail.com",
+            ],
+        },
+        Message={
+            "Body": {
+                "Text": {
+                    "Charset": CHARSET,
+                    "Data": DATA,
+                }
+            },
+            "Subject": {
+                "Charset": CHARSET,
+                "Data": "SPY Fibonacci Retracement Tracker",
+            },
+        },
+        Source="FibonacciiRetracement@gmail.com",
+    )
+
+def send_daily_report(level_count, sell_count, loss_count):
+    ses_client = boto3.client("ses", region_name="us-east-1")
+    CHARSET = "UTF-8"
+    if level_count == 0:
+        DATA = "For " + str(date.today()) + ", the SPY retracement tracker detected no retracements, so there is no data to display."
+    else:
+        DATA = "For " +  str(date.today()) + ", the SPY retracement tracker detected: \n" + "\tRetracement Level Hit: " + str(level_count) + " times\n\tSell Level Hit: " + str(sell_count) + " times\n\tLoss Level Hit: " + str(loss_count) + " times" 
+        DATA += "\n\nPercent of levels that resulted in profitable sells: " + str((sell_count/level_count) * 100) + "%\nPercent of levels that resulted in loss sells: " + str((loss_count/level_count) * 100) + "%"
+    response = ses_client.send_email(
+        Destination={
+            "ToAddresses": [
+                "amol@nirenshah.com", "reviewstopclass@gmail.com",
+            ],
+        },
+        Message={
+            "Body": {
+                "Text": {
+                    "Charset": CHARSET,
+                    "Data": DATA,
+                }
+            },
+            "Subject": {
+                "Charset": CHARSET,
+                "Data": "Daily Report: SPY Fibonacci Retracement Tracker",
+            },
+        },
+        Source="FibonacciiRetracement@gmail.com",
+    )
 
 ticker = "SPY"
 level = 0
-
+sell_level = 0
+loss_level = 0
+level_count = 0
+sell_count = 0
+loss_count = 0
+notify_red_zone = False
 data = getData()
 min = data[-1:]['Low']
+current = data[-1:]['Low']
 max = data[-1:]['High']
 swing = max[0] - min[0]
 
@@ -52,9 +151,11 @@ while True:
     data = getData()
     current = data[-1:]['Low']
     current_max = data[-1:]['High']
-    # print(min)
-    # print(current)
-    # print("--------")
+
+    current_time = datetime.datetime.now()
+    if current_time.hour == 21 and current_time.minute > 55:
+        send_daily_report(level_count, sell_count, loss_count)
+
     if min[0] > current[0]:
         min = current
         max = current_max
@@ -64,24 +165,33 @@ while True:
         max = current_max
         swing = max[0] - min[0]
         level = calcRetracements(swing)
-    print(level)
-    # print(current)
-    # print(level)
-    # print(min)
-    # print(max)
+
 
     if current[0] < level:
         if swing >= max[0] * 0.00625:
-            print("****************Hit .5 Level***************************")
+            send_buy_email(ticker, current[0], min, max)
+            sell_level = calcSellRetracement(swing)
+            loss_level = calcLossRetracement(swing)
+            notify_red_zone = True
             min = current
             max = current_max
             swing = 0
             level = calcRetracements(swing)
-            send_level_email(ticker, current[0], min, max)
-        # else:
-        #     print("\\\\\\\\\\\\\\\\\\\\\\\Hit but not enough swing////////////////////////")
-        #     send_email_low_swing()
-
+            level_count += 1
+            
+    if current[0] > sell_level and sell_level > level and notify_red_zone:
+        send_sell_email(ticker, current[0])
+        sell_level = 0
+        loss_level = 0
+        notify_red_zone = False
+        sell_count += 1
+        
+    if current[0] < loss_level and notify_red_zone:
+        send_loss_email(ticker, current[0])
+        sell_level = 0
+        loss_level = 0
+        notify_red_zone = False
+        loss_count += 1
         
         
 
